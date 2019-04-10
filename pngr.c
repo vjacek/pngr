b3fd21ab0cd9578963a8c50ab96c78a2b169b2cf
Victor Jacek <victor.jacek@ecurv.com>
2019-04-09 23:56:31 -0400

    input

--- pngr.c
+++ pngr.c
@@ -8,16 +8,33 @@ int main(void) {
   cbreak();
   noecho();
 
-  clear();
+  //clear();
+  int max_row, max_col;
+  getmaxyx(stdscr, max_row, max_col);
 
+  ///////// todo: remove when done testing
+  mvprintw(1, 1, "row: ");
+  mvprintw(2, 2, "%i", max_row);
+  mvprintw(3, 3, "col: ");
+  mvprintw(4, 4, "%i", max_col);
+  refresh();  
 
+  int command = getch();
+  while (command != 'q') {
 
+ 
+    if (command == 'a') {
+      mvprintw(max_row-1, 0, "aaaaaaaaaaa");
+      refresh();
+    }
 
-  printf("hello world \r\n");
-
-
-  getch();
-  endwin();
-
-  exit(0);
+    
+    else if (command == 'q') {
+      endwin();
+      return 0;
+    }
+ 
+    command = getch();
+  }
+  
 }
